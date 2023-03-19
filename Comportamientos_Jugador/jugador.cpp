e110843edc23e9f603c8a85dc7b7a6e1a31ab0fa
lauralsoraluce <lazarosoraluce@gmail.com>
2023-03-19 19:08:31 +0100

    Primeros cambios jugador

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -34,9 +34,343 @@ Action ComportamientoJugador::think(Sensores sensores){
 	cout << "Vida: " << sensores.vida << endl;
 	cout << endl;
 
+	int a;
+	switch(last_action){
+		case actFORWARD: //Si me muevo recto
+			switch(current_state.brujula){ 
+				case norte: current_state.fil--; break; //Si estaba mirando al norte
+				case noreste: current_state.fil--; current_state.col++; break;
+				case este: current_state.col++; break;
+				case sureste: current_state.fil++; current_state.col++; break;
+				case sur: current_state.fil++; break;
+				case suroeste: current_state.fil++; current_state.col--; break;
+				case oeste: current_state.col--; break;
+				case noroeste: current_state.fil--; current_state.col--; break;
+			}
+			break;
+		case actTURN_SL:
+			a = current_state.brujula;
+			a = (a+7) % 8; // Equivalente a (a-1)%8 -> giro 45 left
+			current_state.brujula = static_cast<Orientacion>(a);
+			break;
+		case actTURN_SR:
+		a = current_state.brujula;
+			a = (a+1) % 8;
+			current_state.brujula = static_cast<Orientacion>(a);
+			break;
+		case actTURN_BL:
+		a = current_state.brujula;
+			a = (a+5) % 8;
+			current_state.brujula = static_cast<Orientacion>(a);
+			break;
+		case actTURN_BR:
+		a = (a+3) % 8;
+			current_state.brujula = static_cast<Orientacion>(a);
+			break;
+	}
 
-	// Determinar el efecto de la ultima accion enviada
+	// 4
+	if(sensores.posF != -1 and !bien_situado){
+		current_state.fil = sensores.posF;
+		current_state.col = sensores.posC;
+		current_state.brujula = sensores.sentido;
+		bien_situado = true;
+	}
+
+	if (last_action == actFORWARD) { //Si he cambiado de casilla, apunto que he visitado la casilla actual. 
+		mapaVisitas[current_state.fil][current_state.col]++;
+	}
+
+	if(bien_situado){
+		mapaResultado[current_state.fil][current_state.col] = sensores.terreno[0];
+	}
+
+	// INTENTO DE ACTUALIZACIÓN DE MOVIMIENTO EN EL MAPA
+	menosVisitada.fil=-1;
+	menosVisitada.col=-1;
+	switch(current_state.brujula){ 
+		case norte: 
+			if (current_state.fil-1>=0) {
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col; 
+				proximaAccion = actFORWARD;
+			}
+			if (current_state.fil-1>=0 and current_state.col+1<100 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil-1][current_state.col+1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col+1;
+				proximaAccion = actTURN_SR;
+			}
+			if (current_state.fil-1>=0 and current_state.col-1>=0 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil-1][current_state.col-1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col-1;
+				proximaAccion = actTURN_SL;
+			}
+			if (current_state.fil+1<100 and current_state.col+1<100 and
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil+1][current_state.col+1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil+1;
+				menosVisitada.col = current_state.col+1;
+				proximaAccion = actTURN_BR;
+			}
+			if (current_state.fil-1>=0 and current_state.col-1>=0 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil-1][current_state.col-1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col-1;
+				proximaAccion = actTURN_BL;
+			}
+			break; //Si estaba mirando al norte
+		case noreste:
+			if (current_state.fil-1>=0 and current_state.col+1<100) {
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col+1; 
+				proximaAccion = actFORWARD;
+			}
+			if (current_state.col+1<100 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil][current_state.col+1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil;
+				menosVisitada.col = current_state.col+1;
+				proximaAccion = actTURN_SR;
+			}
+			if (current_state.fil-1>=0 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil-1][current_state.col]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col;
+				proximaAccion = actTURN_SL;
+			}
+			if (current_state.fil+1<100 and
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil+1][current_state.col]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil+1;
+				menosVisitada.col = current_state.col;
+				proximaAccion = actTURN_BR;
+			}
+			if (current_state.col-1>=0 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil][current_state.col-1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil;
+				menosVisitada.col = current_state.col-1;
+				proximaAccion = actTURN_BL;
+			} 
+			break;
+		case este:
+			if (current_state.col+1<100) {
+				menosVisitada.fil = current_state.fil;
+				menosVisitada.col = current_state.col+1; 
+				proximaAccion = actFORWARD;
+			}
+			if (current_state.fil+1<100 and current_state.col+1<100 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil+1][current_state.col+1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil+1;
+				menosVisitada.col = current_state.col+1;
+				proximaAccion = actTURN_SR;
+			}
+			if (current_state.fil-1>=0 and current_state.col+1<100 and
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil-1][current_state.col+1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col+1;
+				proximaAccion = actTURN_SL;
+			}
+			if (current_state.fil+1<100 and current_state.col-1>=0 and
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil+1][current_state.col-1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil+1;
+				menosVisitada.col = current_state.col-1;
+				proximaAccion = actTURN_BR;
+			}
+			if (current_state.fil-1>=0 and current_state.col-1>=0 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil-1][current_state.col-1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col-1;
+				proximaAccion = actTURN_BL;
+			} 
+			break;
+		case sureste:
+			if (current_state.fil+1<100 and current_state.col+1<100) {
+				menosVisitada.fil = current_state.fil+1;
+				menosVisitada.col = current_state.col+1; 
+				proximaAccion = actFORWARD;
+			}
+			if (current_state.fil+1<100 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil+1][current_state.col]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil+1;
+				menosVisitada.col = current_state.col;
+				proximaAccion = actTURN_SR;
+			}
+			if (current_state.col+1<100 and
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil][current_state.col+1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil;
+				menosVisitada.col = current_state.col+1;
+				proximaAccion = actTURN_SL;
+			}
+			if (current_state.col-1>=0 and
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil][current_state.col-1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil;
+				menosVisitada.col = current_state.col-1;
+				proximaAccion = actTURN_BR;
+			}
+			if (current_state.fil-1>=0 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil-1][current_state.col]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col;
+				proximaAccion = actTURN_BL;
+			} 
+			break;
+		case sur:
+			if (current_state.fil+1<100) {
+				menosVisitada.fil = current_state.fil+1;
+				menosVisitada.col = current_state.col; 
+				proximaAccion = actFORWARD;
+			}
+			if (current_state.fil+1<100 and current_state.col-1>=0 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil+1][current_state.col-1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil+1;
+				menosVisitada.col = current_state.col-1;
+				proximaAccion = actTURN_SR;
+			}
+			if (current_state.fil+1<100 and current_state.col+1<100 and
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil+1][current_state.col+1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil+1;
+				menosVisitada.col = current_state.col+1;
+				proximaAccion = actTURN_SL;
+			}
+			if (current_state.fil-1>=0 and current_state.col-1>=0 and
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil-1][current_state.col-1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col-1;
+				proximaAccion = actTURN_BR;
+			}
+			if (current_state.fil-1>=0 and current_state.col+1<100 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil-1][current_state.col+1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col+1;
+				proximaAccion = actTURN_BL;
+			} 
+			break;
+		case suroeste:
+			if (current_state.fil+1<100 and current_state.col-1>=0) {
+				menosVisitada.fil = current_state.fil+1;
+				menosVisitada.col = current_state.col-1; 
+				proximaAccion = actFORWARD;
+			}
+			if (current_state.col-1>=0 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil][current_state.col-1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil;
+				menosVisitada.col = current_state.col-1;
+				proximaAccion = actTURN_SR;
+			}
+			if (current_state.fil+1<100 and
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil+1][current_state.col]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil+1;
+				menosVisitada.col = current_state.col;
+				proximaAccion = actTURN_SL;
+			}
+			if (current_state.fil-1>=0 and
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil-1][current_state.col]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col;
+				proximaAccion = actTURN_BR;
+			}
+			if (current_state.col+1<100 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil][current_state.col+1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil;
+				menosVisitada.col = current_state.col+1;
+				proximaAccion = actTURN_BL;
+			} 
+			break;
+		case oeste:
+			if (current_state.col-1>=0) {
+				menosVisitada.fil = current_state.fil;
+				menosVisitada.col = current_state.col-1; 
+				proximaAccion = actFORWARD;
+			}
+			if (current_state.fil-1>=0 and current_state.col-1>=0 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil-1][current_state.col-1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col-1;
+				proximaAccion = actTURN_SR;
+			}
+			if (current_state.fil+1<100 and current_state.col-1>=0 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil+1][current_state.col-1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil+1;
+				menosVisitada.col = current_state.col-1;
+				proximaAccion = actTURN_SL;
+			}
+			if (current_state.fil-1>=0 and current_state.col+1<100 and
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil-1][current_state.col+1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col+1;
+				proximaAccion = actTURN_BR;
+			}
+			if (current_state.fil+1<100 and current_state.col+1<100 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil+1][current_state.col+1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil+1;
+				menosVisitada.col = current_state.col+1;
+				proximaAccion = actTURN_BL;
+			} 
+			break;
+		case noroeste:
+			if (current_state.fil-1>=0 and current_state.col-1>=0) {
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col-1; 
+				proximaAccion = actFORWARD;
+			}
+			if (current_state.fil-1>=0 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil-1][current_state.col]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil-1;
+				menosVisitada.col = current_state.col;
+				proximaAccion = actTURN_SR;
+			}
+			if (current_state.col-1>=0 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil][current_state.col-1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil;
+				menosVisitada.col = current_state.col-1;
+				proximaAccion = actTURN_SL;
+			}
+			if (current_state.col+1<100 and
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil][current_state.col+1]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil;
+				menosVisitada.col = current_state.col+1;
+				proximaAccion = actTURN_BR;
+			}
+			if (current_state.fil+1<100 and 
+			(menosVisitada.fil=-1 || mapaVisitas[current_state.fil+1][current_state.col]<mapaVisitas[menosVisitada.fil][menosVisitada.col])){
+				menosVisitada.fil = current_state.fil+1;
+				menosVisitada.col = current_state.col;
+				proximaAccion = actTURN_BL;
+			} 
+			break;
+	}
+	accion = proximaAccion;
+	last_action=accion;
+
+	/*
+	if((sensores.terreno[2] == 'T' or sensores.terreno[2] == 'S' or sensores.terreno[2] == 'G') and sensores.superficie[2] == '_'){
+		accion = actFORWARD;
+	}
+	else if(!girar_derecha){
+		if (!giro_grande) {
+			accion = actTURN_SL;
+		}
+		else {
+			accion = actTURN_BL;
+		}
+		giro_grande = (rand()%2 == 0);
+		girar_derecha = (rand()%2 == 0);
+	}
+	else{
+		if (!giro_grande) {
+			accion = actTURN_SR;
+		}
+		else {
+			accion = actTURN_BR;
+		}
+		giro_grande = (rand()%2 == 0);
+		girar_derecha = (rand()%2 == 0);
+	}
+
+	last_action = accion;
+	*/
+
 	return accion;
+	
 }
 
 int ComportamientoJugador::interact(Action accion, int valor){
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -4,12 +4,38 @@
 #include "comportamientos/comportamiento.hpp"
 using namespace std;
 
+struct state{
+  int fil;
+  int col;
+  Orientacion brujula;
+};
+
+struct casilla{
+  int fil;
+  int col;
+};
+
+
 class ComportamientoJugador : public Comportamiento{
 
   public:
     ComportamientoJugador(unsigned int size) : Comportamiento(size){
       // Constructor de la clase
       // Dar el valor inicial a las variables de estado
+      last_action = actIDLE; //No se hizo nada en el instante anterior a empezar la simulación
+      //Orientacion = norte;
+      current_state.fil = current_state.col = 99;
+      current_state.brujula = norte;
+
+      girar_derecha = false;
+      giro_grande = false;
+      bien_situado = false;
+
+      vector<unsigned int> aux(100, 0);
+      for (unsigned int i = 0; i < 100; i++)
+      {
+        mapaVisitas.push_back(aux);
+      }
     }
 
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
@@ -21,6 +47,22 @@ class ComportamientoJugador : public Comportamiento{
   private:
   
   // Declarar aquí las variables de estado
+  Action last_action; 
+  // Orientacion brujula;
+
+  state current_state; 
+  /* Importante para los niveles del juego en los que no se dispone de sensores de orientación 
+  y es el propio jugador el que tiene que guardar esta información */
+
+  bool girar_derecha;
+  bool giro_grande; // Para que también se puedan hacer los giros de 135 al moverse por el terreno
+  bool bien_situado;
+
+
+  // INTENTO DE ACTUALIZACIÓN DE MAPA CUANDO LLEGUEMOS A LA CASILLA G 
+    vector< vector< unsigned int> > mapaVisitas;
+  casilla menosVisitada;
+  Action proximaAccion;
 
 };
 
